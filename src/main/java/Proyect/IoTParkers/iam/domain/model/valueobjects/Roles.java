94e0c72a7f4a283be9c5dfc0108e3cc52134215d
Nicolas <122683587+nicxlxs@users.noreply.github.com>
2025-10-22 16:47:54 -0500

    feature(iam): add new roles

--- src/main/java/Proyect/IoTParkers/iam/domain/model/valueobjects/Roles.java
+++ src/main/java/Proyect/IoTParkers/iam/domain/model/valueobjects/Roles.java
@@ -1,7 +1,9 @@
 package Proyect.IoTParkers.iam.domain.model.valueobjects;
 
 public enum Roles {
+    ROLE_PLATFORM_ADMIN,
+    ROLE_MERCHANT_ADMIN,
+    ROLE_MERCHANT_EMPLOYEE,
     ROLE_USER,
-    ROLE_ADMIN,
-    ROLE_INSTRUCTOR
+    ROLE_AUDITOR
 }
