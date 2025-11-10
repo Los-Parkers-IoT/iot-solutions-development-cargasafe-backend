80ac78d97c047124cab3900057c7497386c14ff5
SgtGarro <marcelogarro137@gmail.com>
2025-11-10 08:16:13 -0500

    build: expose application for all origins

--- src/main/java/Proyect/IoTParkers/shared/infrastructure/configuration/WebConfig.java
+++ src/main/java/Proyect/IoTParkers/shared/infrastructure/configuration/WebConfig.java
@@ -16,8 +16,7 @@ public class WebConfig {
                 registry
                         .addMapping("/**")
                         .allowedOrigins(
-                                "http://localhost:4200",
-                                "http://localhost:5173"
+                                "*"
                         )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                         .allowedHeaders("*")
